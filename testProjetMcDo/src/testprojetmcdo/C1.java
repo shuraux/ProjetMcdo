7b7e8ee041bb5f3c63dc69ece855df33c819c0d9
shuraux <sylvain.huraux@outlook.fr>
2016-11-07 10:35:50 +0100

    test commit par Sylvain

---
+++ testProjetMcDo/src/testprojetmcdo/C1.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testprojetmcdo;
+
+/**
+ *
+ * @author Sylvain HURAUX <your.name at your.org>
+ */
+public class C1 {
+    
+    public static void c1(){
+        System.out.println("C1 version 1 de E1");
+    }
+}
