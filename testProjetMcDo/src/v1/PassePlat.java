c6bd9502104fafbb8ea8fcbbdb93de2baf9b13dd
shuraux <sylvain.huraux@outlook.fr>
2016-11-07 16:10:12 +0100

    v1 taf commit par Sylvain

--- testProjetMcDo/src/v1/PassePlat.java
+++ testProjetMcDo/src/v1/PassePlat.java
@@ -10,5 +10,30 @@ package v1;
  * @author Sylvain HURAUX <your.name at your.org>
  */
 public class PassePlat {
+    private int nbSandwichs;    //le nb de sandwichs dispo dans le passe-plat init à 0
+    
+    public PassePlat(){
+        this.nbSandwichs=0;
+    }
+    
+    public static void main(String[] args) {
+        PassePlat passePlat = new PassePlat();
+        
+        Thread p1 = new Thread(new Producteur(passePlat, 1));
+        Thread p2 = new Thread(new Producteur(passePlat, 2));
+        p1.start();
+        p2.start();
+    }
+
+    /**
+     * @return the nbSandwichs
+     */
+    public synchronized int getNbSandwichs() {
+        return this.nbSandwichs;
+    }
+    
+    public synchronized void ajouterSandwich(){
+        this.nbSandwichs=this.nbSandwichs+1;
+    }
     
 }
---
+++ testProjetMcDo/src/v1/Producteur.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package v1;
+
+/**
+ *
+ * @author Sylvain HURAUX <your.name at your.org>
+ */
+public class Producteur implements Runnable{
+    //private float ratio;
+   private int numero;
+   PassePlat passePlat;
+    
+    public Producteur(PassePlat passePlat, int numero){
+        //this.ratio=(float)(30.0*Math.random());
+        this.numero=numero;
+        this.passePlat=passePlat;
+    }
+    
+    public void run(){
+        for(int i = 0; i < 10; i++){
+            System.out.println("Sandwich créé par le producteur " + numero);
+            passePlat.ajouterSandwich();
+            System.out.println(passePlat.getNbSandwichs());
+        }
+    }
+}
