ae1c01ac69850cfa6276c0fe47450c1e66529fc6
Guillaume9394 <simonet.guillaume@hotmail.fr>
2016-11-07 11:19:00 +0100

    Test Commit Guillaume

--- testProjetMcDo/src/testprojetmcdo/C1.java
+++ testProjetMcDo/src/testprojetmcdo/C1.java
@@ -14,5 +14,7 @@ public class C1 {
     public static void c1(){
         System.out.println("C1 version 1 de E1");
         System.out.println("C1 version 2 de E2");
+        System.out.println("C1 version 1 par E1");
+        System.out.println("C2 version 2 par E1");
     }
 }
